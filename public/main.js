583b9bde3efa0121fe932c4ab86cee52ba7af08a
Santiago Franco <74748049+SantiagoFrancoDev@users.noreply.github.com>
2021-03-17 11:29:59 -0300

    Add files via upload

---
+++ public/main.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("carrito");
+
+btn.addEventListener("click", pincharon);
+
+function pincharon() {
+  alert("hola");
+}
+
+let comprar = document.querySelectorAll("button");
+comprar.addEventListener("click", compra);
